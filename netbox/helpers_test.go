d45316503580571b4681141971bd196af1fefa57
Hendrik Sokolowski <h.sokolowski@hosting.de>
2018-08-31 15:51:30 +0200

    Add test for helper

---
+++ netbox/helpers_test.go
@@ -0,0 +1,135 @@
+package netbox
+
+import (
+	"testing"
+
+	"github.com/hosting-de-labs/go-netbox-client/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGenerateVMComment(t *testing.T) {
+	cases := []struct {
+		host   types.VirtualServer
+		result string
+	}{
+		{
+			types.VirtualServer{
+				Resources: types.VirtualServerResources{
+					Disks: []types.VirtualServerDisk{
+						types.VirtualServerDisk{
+							Size: 20,
+						},
+					},
+				},
+			},
+			`--- NETBOX SYNC: DO NOT MODIFY ---
+--- NETBOX SYNC: DO NOT MODIFY ---`,
+		},
+		{
+			types.VirtualServer{
+				Resources: types.VirtualServerResources{
+					Disks: []types.VirtualServerDisk{
+						types.VirtualServerDisk{
+							Size: 20,
+						},
+						types.VirtualServerDisk{
+							Size: 10,
+						},
+					},
+				},
+			},
+			`--- NETBOX SYNC: DO NOT MODIFY ---
+Additional disks:
+Size: 10 MBytes
+--- NETBOX SYNC: DO NOT MODIFY ---`,
+		},
+		{
+			types.VirtualServer{
+				Resources: types.VirtualServerResources{
+					Disks: []types.VirtualServerDisk{
+						types.VirtualServerDisk{
+							Size: 20,
+						},
+						types.VirtualServerDisk{
+							Size: 10,
+						},
+						types.VirtualServerDisk{
+							Size: 30,
+						},
+						types.VirtualServerDisk{
+							Size: 40,
+						},
+					},
+				},
+			},
+			`--- NETBOX SYNC: DO NOT MODIFY ---
+Additional disks:
+Size: 10 MBytes
+Size: 30 MBytes
+Size: 40 MBytes
+--- NETBOX SYNC: DO NOT MODIFY ---`,
+		},
+		{
+			types.VirtualServer{
+				Host: types.Host{
+					Comments: []string{
+						"Foo",
+					},
+				},
+			},
+			`--- NETBOX SYNC: DO NOT MODIFY ---
+Comments:
+Foo
+--- NETBOX SYNC: DO NOT MODIFY ---`,
+		},
+		{
+			types.VirtualServer{
+				Host: types.Host{
+					Comments: []string{
+						"Foo",
+					},
+				},
+				Resources: types.VirtualServerResources{
+					Disks: []types.VirtualServerDisk{
+						types.VirtualServerDisk{
+							Size: 20,
+						},
+					},
+				},
+			},
+			`--- NETBOX SYNC: DO NOT MODIFY ---
+Comments:
+Foo
+--- NETBOX SYNC: DO NOT MODIFY ---`,
+		},
+		{
+			types.VirtualServer{
+				Host: types.Host{
+					Comments: []string{
+						"Foo",
+					},
+				},
+				Resources: types.VirtualServerResources{
+					Disks: []types.VirtualServerDisk{
+						types.VirtualServerDisk{
+							Size: 20,
+						},
+						types.VirtualServerDisk{
+							Size: 10,
+						},
+					},
+				},
+			},
+			`--- NETBOX SYNC: DO NOT MODIFY ---
+Comments:
+Foo
+Additional disks:
+Size: 10 MBytes
+--- NETBOX SYNC: DO NOT MODIFY ---`,
+		},
+	}
+
+	for _, testcase := range cases {
+		assert.Equal(t, testcase.result, generateVMComment(&testcase.host))
+	}
+}
