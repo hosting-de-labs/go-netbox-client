2b9af38bd05a872543a79539031313d6f11c55fe
Hendrik Sokolowski <h.sokolowski@hosting.de>
2020-02-26 13:56:40 +0100

    Remove unused methods

--- types/common.go
+++ types/common.go
@@ -4,14 +4,18 @@ import (
 	"encoding/hex"
 	"reflect"
 
-	"github.com/hosting-de-labs/go-netbox/netbox/models"
-
 	"golang.org/x/crypto/sha3"
 )
 
 //HashableEntity has a method to return a string that stays the same when the entity wasn't changed
 type HashableEntity interface {
 	GetHashableString() string
+}
+
+//CommonEntity is a general object that should be extended by every Entity that interfaces with Netbox
+type CommonEntity struct {
+	OriginalEntity interface{} //Deprecated
+	Metadata       Metadata
 }
 
 //Metadata contain information that are relevant to communicate with Netbox
@@ -21,37 +25,15 @@ type Metadata struct {
 	EntityType   reflect.Type
 }
 
-type NetboxEntity struct {
-	entity     interface{}
-	entityType reflect.Type
-}
-
-func (n NetboxEntity) DcimDevice() models.Device {
-	return n.entity.(models.Device)
-}
-
-func (n NetboxEntity) DcimDeviceWithConfigContext() models.DeviceWithConfigContext {
-	return n.entity.(models.DeviceWithConfigContext)
-}
-
 //GetEntity returns the entity and
-func (m Metadata) GetEntity() NetboxEntity {
-	return NetboxEntity{
-		entity:     m.NetboxEntity,
-		entityType: m.EntityType,
-	}
+func (m Metadata) GetEntity() (entity interface{}, entityType reflect.Type) {
+	return m.NetboxEntity, m.EntityType
 }
 
 //SetEntity stores a netbox entity with its type
 func (m *Metadata) SetEntity(entity interface{}) {
 	m.NetboxEntity = entity
 	m.EntityType = reflect.TypeOf(entity)
-}
-
-//CommonEntity is a general object that should be extended by every Entity that interfaces with Netbox
-type CommonEntity struct {
-	OriginalEntity interface{}
-	Metadata       Metadata
 }
 
 //GetIdentifier returns a hash value made from the hashable string
