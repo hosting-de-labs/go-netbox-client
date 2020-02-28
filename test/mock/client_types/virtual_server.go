6fa2943d2e5550f28a2ecbc72fe20d0715496908
Hendrik Sokolowski <h.sokolowski@hosting.de>
2020-02-28 17:45:30 +0100

    Add more mocking methods

---
+++ test/mock/client_types/virtual_server.go
@@ -0,0 +1,13 @@
+package client_types
+
+import "github.com/hosting-de-labs/go-netbox-client/types"
+
+func MockVirtualServer() types.VirtualServer {
+	vm := types.NewVirtualServer()
+	vm.Hypervisor = "hypervisor1"
+	vm.Resources.Cores = 4
+	vm.Resources.Disks = []types.VirtualServerDisk{{Size: 10}}
+
+	vm.Metadata.NetboxEntity = vm.Copy()
+	return *vm
+}
