3f4cec252f2a750d2d5ff16063056dc8314d1671
Hendrik Sokolowski <h.sokolowski@hosting.de>
2020-07-03 12:38:52 +0200

    Extend custom fields code and add more tests

--- types/custom_fields.go
+++ types/custom_fields.go
@@ -1,18 +1,69 @@
 package types
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
-type CustomFields struct{}
-
-func (cf CustomFields) getMap() interface{} {
-	var itf interface{}
-	itf = cf
-
-	return itf.(interface{})
+type CustomFields struct {
+	ids    map[string]int
+	fields map[string]string
 }
 
-func (cf *CustomFields) Value(key string) (val interface{}) {
-	fmt.Printf("%s\n\n%+v\n\n", key, cf)
+//Load accepts and parses an interface representing go-netbox compatible custom fields
+func (c *CustomFields) Load(cf interface{}) (err error) {
+	if cf == nil {
+		return fmt.Errorf("customfields is nil")
+	}
+
+	c.ids = make(map[string]int)
+	c.fields = make(map[string]string)
+
+	for k, f := range cf.(map[string]interface{}) {
+		switch f.(type) {
+		case map[string]interface{}:
+			f := f.(map[string]interface{})
+			if _, ok := f["Val"]; !ok {
+				return fmt.Errorf("invalid custom fields: no Val field")
+			}
+
+			if _, ok := f["Label"]; !ok {
+				return fmt.Errorf("invalid custom fields: no Label field")
+			}
+
+			c.ids[k] = f["Val"].(int)
+			c.fields[k] = f["Label"].(string)
+
+		case string:
+			c.fields[k] = f.(string)
+
+		default:
+			return fmt.Errorf("invalid custom fields: field of type %s found", reflect.TypeOf(f))
+		}
+	}
 
 	return nil
+}
+
+func (c *CustomFields) Val(key string) (val *string) {
+	f, ok := c.fields[key]
+	if !ok {
+		return nil
+	}
+
+	return &f
+}
+
+func (c *CustomFields) ValMap() map[string]interface{} {
+	out := make(map[string]interface{}, len(c.fields))
+
+	for k, v := range c.fields {
+		if id, ok := c.ids[k]; ok {
+			out[k] = id
+		} else {
+			out[k] = v
+		}
+	}
+
+	return out
 }
---
+++ types/custom_fields_test.go
@@ -0,0 +1,52 @@
+package types_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/hosting-de-labs/go-netbox-client/types"
+)
+
+func createNetboxCustomFields() interface{} {
+	fields := make(map[string]interface{}, 2)
+
+	extfield := make(map[string]interface{}, 2)
+	extfield["Val"] = 1
+	extfield["Label"] = "Val 1"
+
+	fields["ext_field"] = extfield
+	fields["string_field"] = "string"
+
+	var out interface{}
+	out = fields
+
+	return out
+}
+
+func TestCustomFields_Load(t *testing.T) {
+	cf := createNetboxCustomFields()
+	c := types.CustomFields{}
+	assert.Nil(t, c.Load(cf))
+}
+
+func TestCustomFields_Val(t *testing.T) {
+	cf := createNetboxCustomFields()
+	c := types.CustomFields{}
+	assert.Nil(t, c.Load(cf))
+
+	assert.NotNil(t, c.Val("string_field"))
+	assert.Equal(t, "string", *c.Val("string_field"))
+	assert.Equal(t, "Val 1", *c.Val("ext_field"))
+}
+
+func TestCustomFields_ValMap(t *testing.T) {
+	cf := createNetboxCustomFields()
+	c := types.CustomFields{}
+	assert.Nil(t, c.Load(cf))
+
+	m := c.ValMap()
+
+	assert.Equal(t, "string", m["string_field"].(string))
+	assert.Equal(t, 1, m["ext_field"].(int))
+}
