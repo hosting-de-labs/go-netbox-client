21ea62fdb35381cf0dc8582479e8a992afe3ff90
Hendrik Sokolowski <h.sokolowski@hosting.de>
2020-08-10 15:16:32 +0200

    Ignore CommenEntity when comparing network interfaces

--- types/network_interface.go
+++ types/network_interface.go
@@ -119,7 +119,7 @@ func NewNetworkInterface() *NetworkInterface {
 
 //IsEqual compares the current NetworkInterface object against another NetworkInterface
 func (netIf NetworkInterface) IsEqual(netIf2 NetworkInterface) bool {
-	if !utils.CompareStruct(netIf, netIf2, []string{}, []string{"IPAddresses"}) {
+	if !utils.CompareStruct(netIf, netIf2, []string{}, []string{"CommonEntity", "IPAddresses"}) {
 		return false
 	}
 
